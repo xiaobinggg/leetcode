ce9c48d7fac5b86c8540ac1526c15e92467fddbb
xiaobinggg <xiaobinggg@gmail.com>
2019-08-01 21:01:03 +0800

    bserch

---
+++ src/algorithms/search/Bsearch.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package algorithms.search;
+
+/**@Description: 二分查找
+ * @author:LiuXiaoBing 
+ * @date: 2019年8月1日 下午8:25:20
+ */
+public class Bsearch {
+	/***
+	 * 1、查找第一个值等于给定值的元素
+	 * 2、查找最后一个值等于给定值的元素
+	 * 3、查找第一个大于等于给定值的元素
+	 * 4、查找最后一个小于等于给定值的元素
+	 */
+	
+	public static void findFirst() {
+		int[] arr = new int[] {1,3,4,5,6,8,8,8,11,18};
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
